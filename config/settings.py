5a3488760615f45af4939e9a30ec9af630ef8aa9
Ryo730 <miyamoto073015@gmail.com>
2020-09-02 14:48:11 +0900

    c

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
+    ##moneybookではない項目（＊）
+    ##'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -65,9 +66,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins':[
-                    'bootstrap4.templatetags.bootstrap4',
-            ],
+            ##'builtins':[
+                    ##'bootstrap4.templatetags.bootstrap4',
+            ##],
         },
     },
 ]
